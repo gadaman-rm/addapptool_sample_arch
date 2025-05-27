30926b7c317bc7d709780fbc5f7484fb0c6d2275
gadaman <randm2001@gmail.com>
2025-05-27 16:47:54 +0330

    Refactor application structure by introducing a new SamplePage component and updating routing in app.ts. Replace main.ts with app.ts as the entry point in index.html. Add Material Design dependencies and remove unused files related to places. Include sample.scss for styling the new component.

--- app.ts
+++ app.ts
@@ -1,1 +1,22 @@
+import { html, render } from "lit";
 import { router } from "./src/Router";
+import { SamplePage } from "./src/view/pages/sample/sample";
+
+const appContainer = document.getElementById("appContainer") as HTMLDivElement;
+
+router.addRoute("/", () => {
+    const samplePage = new SamplePage();
+    samplePage.setup();
+    samplePage.load();
+});
+
+router.addRoute("/404", () => {
+    const page = html`<h1>Not found</h1>`;
+    render(page, appContainer);
+});
+
+async function init() {
+    router.navigateTo("/");
+}
+
+init();
